a8d362ed1205e5537e8e2aefab99fd75e2ac1de3
mohamed-elmashad <mohamedapple@hotmail.com>
2024-03-19 21:06:20 -0700

    Fix 91

--- 91translate.py
+++ 91translate.py
@@ -12,25 +12,19 @@ parser.add_argument('-a', '--anti', action='store_true',
 args = parser.parse_args()
 
 for name, seq in mcb185.read_fasta(args.file):
-	protein_seq = genomeutils.translate(seq)
+	if not args.anti:
+		protein_seq = genomeutils.translate(seq)
+	else:
+		reverse = genomeutils.reverse_complement(seq)
+		protein_seq = genomeutils.translate(reverse)
+		
 	# find first methionine
 	start_index = protein_seq.find('M')
-	# if not found, check for leucine
-	if start_index == -1:
-		start_index = protein_seq.find('L')
 	if start_index != -1:
 		protein_seq_trimmed = protein_seq[start_index:]
 		# trim off stop codon
 		end_index = protein_seq_trimmed.find('*')
 		protein_seq_trimmed = protein_seq_trimmed[:end_index]
 		if len(protein_seq_trimmed) >= args.min:
-			if not args.anti:
-				print('>' + name)
-				genomeutils.pretty_print(protein_seq_trimmed)
-			else:
-				print('>' + name + ' (strand)')
-				genomeutils.pretty_print(protein_seq_trimmed)
-
-				print('>' + name + ' (anti-parallel strand)')
-				translated_seq = genomeutils.translate(genomeutils.reverse_complement(seq))
-				genomeutils.pretty_print(translated_seq[start_index:])
+			print('>' + name)
+			genomeutils.pretty_print(protein_seq_trimmed)
