c1270998ff8a2ce8bd8dbcc3cb90c2cb277cceef
mohamed-elmashad <mohamedapple@hotmail.com>
2024-03-19 22:04:41 -0700

    Fix 74

--- 74genefinder.py
+++ 74genefinder.py
@@ -2,7 +2,6 @@ import genomeutils
 import mcb185
 import sys
 
-
 def find_ORFS(sequence, positive):
 	symbol = ""
 	if positive: 
@@ -15,10 +14,21 @@ def find_ORFS(sequence, positive):
 	for frame in range(3):
 		# iterate over each nucleotide in the frame
 		for i in range(frame, len(sequence)-2, 3):
-			if sequence[i:i+3] == 'ATG':
-				for j in range(i+3, len(sequence)-2, 3):
-					if sequence[j:j+3] in ('TAA', 'TAG', 'TGA'):
-						orfs.append((i+1, j+3, symbol))
+			if sequence[i:i + 3] == 'ATG':
+				for j in range(i + 3, len(sequence)-2, 3):
+					if sequence[j:j + 3] in ('TAA', 'TAG', 'TGA'):
+						start = i + 1
+						end = j + 3
+						overlap = False
+						for o in orfs:
+							existing_start = o[0]
+							existing_end = o[1]
+							if (start <= existing_end and start >= existing_start) or \
+								(end <= existing_end and end >= existing_start):
+								overlap = True
+								break
+						if not overlap:
+							orfs.append((start, end, symbol))
 						break
 	return orfs
 
@@ -27,16 +37,18 @@ def find_cds(sequence, min_orf_length):
 	# Positive strand
 	orfs = find_ORFS(sequence, True)
 	for orf in orfs:
-		if orf[1] - orf[0] >= min_orf_length:
+		start, end, strand = orf
+		if end - start >= min_orf_length:
 			cds_list.append(orf)
+
 	# Negative strand
 	orfs = find_ORFS(genomeutils.reverse_complement(sequence), False)
 	for orf in orfs:
-		if orf[1] - orf[0] >= min_orf_length:
-			cds_list.append((len(sequence)-orf[1]+1, len(sequence)-orf[0]+1, '-'))
+		start, end, strand = orf
+		if end - start >= min_orf_length:
+			cds_list.append((len(sequence)-end + 1, len(sequence)-end + 1, '-'))
 
 	return cds_list
-
 
 fasta_file = sys.argv[1]
 min_orf_length = int(sys.argv[2])
